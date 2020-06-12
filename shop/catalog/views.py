a17e6ccf6212f939a75a915d9aac9a92811cb23f
Erkinumur <erkin.umur94@gmail.com>
2020-06-12 18:02:33 +0600

    Add filters and search

--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render, redirect
 from django.views import generic
+from django.db.models import Q
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy, reverse
 
-from .models import Watch
+from .models import Watch, Group, Brand
 from .forms import AuthUserForm
+
+
+class FilterMixin:
+    def get_categories(self):
+        return Group.objects.all().order_by('name')
+
+    def get_brands(self):
+        return Brand.objects.all().order_by('name')
+
+    def get_min_max_price(self):
+        min_price = Watch.objects.order_by('price').first().price
+        max_price = Watch.objects.order_by('-price').first().price
+        return {'min': min_price, 'max': max_price}
 
 
 class WatchCreateView(generic.CreateView):
@@ -24,7 +38,7 @@ class WatchDeleteView(generic.DeleteView):
     success_url = reverse_lazy('index_page')
 
 
-class WatchListView(generic.ListView):
+class WatchListView(FilterMixin, generic.ListView):
     model = Watch
     template_name='catalog/product_list.html'
 
@@ -42,7 +56,26 @@ class IndexView(generic.ListView):
         return Watch.objects.all().order_by('-create_at')[:4]
 
 
-class SignInView(LoginView):
-    template_name = 'catalog/sign_in.html'
-    form_class = AuthUserForm
-    success_url = reverse_lazy('index_page')
+class FilterListView(FilterMixin, generic.ListView):
+    model = Watch
+    template_name = 'catalog/product_list.html'
+
+    def get_queryset(self):
+        queryset = Watch.objects.filter(
+            Q(group__in=self.request.GET.getlist('category')) |
+            Q(brand__in=self.request.GET.getlist('brand'))
+        )
+        return queryset
+
+
+class SearchProductView(FilterMixin, generic.ListView):
+    model = Watch
+    template_name = 'catalog/product_list.html'
+
+    def get_queryset(self):
+        return Watch.objects.filter(title__icontains=self.request.GET.get('q'))
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['q'] = self.request.GET.get('q')
+        return context
