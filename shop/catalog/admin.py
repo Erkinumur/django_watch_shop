9a19c8e6deadc8105e8b0d2c4bfb1b0dfe56054b
Erkinumur <erkin.umur94@gmail.com>
2020-06-14 01:53:39 +0600

    edit admin.py

--- shop/catalog/admin.py
+++ shop/catalog/admin.py
@@ -2,7 +2,29 @@ from django.contrib import admin
 
 from .models import Brand, Group, Watch, Review
 
-admin.site.register(Brand)
-admin.site.register(Group)
-admin.site.register(Watch)
+# admin.site.register(Brand)
+# admin.site.register(Group)
+# admin.site.register(Watch)
 admin.site.register(Review)
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Group)
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    list_display_links = ('name',)
+    list_filter = ('name',)
+    search_fields = ('name',)
+
+@admin.register(Watch)
+class WatchAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'brand', 'get_group', 'price', 'description', 'image')
+    list_display_links = ('title', 'brand', 'get_group', 'price', 'description')
+    list_filter = ('title', 'brand', 'group', 'price')
+    search_fields = ('title', 'brand__name', 'group__name')
